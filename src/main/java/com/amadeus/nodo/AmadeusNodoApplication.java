25964a22c3165a7696d404cdad48bd2ac61290f8
IsaacBerri <berriisaax18@hotmail.com>
2024-11-07 17:31:31 -0500

    Initial Commit

---
+++ src/main/java/com/amadeus/nodo/AmadeusNodoApplication.java
@@ -0,0 +1,12 @@
+package com.amadeus.nodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusNodoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusNodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amadeus/nodo/AmadeusNodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadeus.nodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusNodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
