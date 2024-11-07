d4deef9893963f0e1341b5411dc3632486f5ef6a
Jhon Arias <arias887@hotmail.com>
2024-11-07 18:34:03 -0500

    :sparkles: Add Hosting Entity

---
+++ src/main/java/com/amadeus/nodo/Models/HostingEntity.java
@@ -0,0 +1,17 @@
+package com.amadeus.nodo.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class HostingEntity {
+    @Id
+    @GeneratedValue (strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    public String name;
+}
