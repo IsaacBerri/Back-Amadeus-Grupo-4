da4024a89b74723b10637e95955a36aa89c0e0b4
LuisaRami2018 <luisa.ramirezrpo10@gmail.com>
2024-11-07 17:58:33 -0500

    :sparkles: Add userEntity

---
+++ src/main/java/com/amadeus/nodo/Service/UserSevice.java
@@ -0,0 +1,35 @@
+package com.amadeus.nodo.Service;
+
+import com.amadeus.nodo.Models.UserEntity;
+import com.amadeus.nodo.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserSevice {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void create(UserEntity userEntity) {
+        userRepository.save(userEntity);
+        System.out.println("El usuario ha sido creado con exito");
+    }
+
+    public List<UserEntity> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserEntity> findById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public String delete(int id) {
+        userRepository.deleteById(id);
+        return "La persona con ID: " + id + " ha sido eliminada";
+    }
+
+}
