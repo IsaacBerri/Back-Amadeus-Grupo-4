cafc5e06f64ce317cc9d212c7f90060f319b7357
LuisaRami2018 <luisa.ramirezrpo10@gmail.com>
2024-11-08 17:47:05 -0500

    :rocket: add constructors userEntity

--- src/main/java/com/amadeus/nodo/Models/UserEntity.java
+++ src/main/java/com/amadeus/nodo/Models/UserEntity.java
@@ -4,14 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class UserEntity {
 
@@ -22,5 +19,14 @@ public class UserEntity {
     @Getter
     private String name;
     private String email;
+
+    public UserEntity(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserEntity(){
+
+    }
 
 }
