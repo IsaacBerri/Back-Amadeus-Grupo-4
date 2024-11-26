da139e591f3464fa01ff146f1c9837d0f2e4f7d0
IsaacBerri <berriisaax18@hotmail.com>
2024-11-26 13:38:12 -0500

    :sparkles: Implementing the default destination option

--- src/main/java/com/amadeus/nodo/Models/Mongo/DestinationsEntity.java
+++ src/main/java/com/amadeus/nodo/Models/Mongo/DestinationsEntity.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -27,24 +25,13 @@ public class DestinationsEntity {
     private Destinity destinationEuropa;
 
     @JsonSerialize
+    @Getter
+    @Setter
     public static class Destinity {
         private String site;
         private String country;
-
-        public String getSite() {
-            return site;
-        }
-
-        public void setSite(String site) {
-            this.site = site;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
+        private String language;
+        private String unmissablePlace;
+        private String typicalFood;
     }
 }
